9f08901f6c3653b02cf8b612c0fa0e2a748e2d07
12PUffS <evgenypech98@gmail.com>
2026-03-17 22:01:44 +0300

    frontend folder init

---
+++ technooark-admin/frontend/src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import './Header.css';
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>хедер</h1>
+    </header>
+  );
+}
---
+++ technooark-admin/frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <h1>Главная страница</h1>
+    </div>
+  );
+}
